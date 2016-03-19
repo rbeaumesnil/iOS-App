fdd3efda4bfa8adeb12e5e33eae01f5797755ce0
tp <tp@tps-macbook-pro.local>
2016-03-19 18:10:05 +0100

    Nouveau repository Nouveau projet

---
+++ iOS-App/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  iOS-App
+//
+//  Created by tp on 14/03/2016.
+//  Copyright © 2016 tp. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+
+}
---
+++ iOS-App/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  iOS-App
+//
+//  Created by tp on 14/03/2016.
+//  Copyright © 2016 tp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func onLoginClick(sender: AnyObject) {
+        
+    }
+    @IBAction func onSignUpClick(sender: AnyObject) {
+        	
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
