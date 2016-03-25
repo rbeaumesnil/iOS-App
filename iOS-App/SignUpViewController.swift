6df3c14ea61258ade19721a294ed2d3323b06f02
tp24 <tp24@macmini2.deptinfo.univ-orleans.fr>
2016-03-25 15:18:46 +0100

    Inscription OK ? A vérifier

--- iOS-App/SignUpViewController.swift
+++ iOS-App/SignUpViewController.swift
@@ -7,28 +7,32 @@
 //
 
 import UIKit
+import CoreData
 
 class SignUpViewController: UIViewController, UITextFieldDelegate {
-    
     
     @IBOutlet weak var mailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var lastnameField: UITextField!
+
     @IBOutlet weak var firstnameField: UITextField!
     
     @IBOutlet weak var phoneField: UITextField!
-
+    
     @IBOutlet weak var addressField: UITextField!
     @IBOutlet weak var signUpButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.mailField.delegate = self;
+        self.mailField.keyboardType = UIKeyboardType.EmailAddress
         self.passwordField.delegate = self;
         self.lastnameField.delegate = self;
         self.firstnameField.delegate = self;
         self.phoneField.delegate = self;
+        self.phoneField.keyboardType = UIKeyboardType.NumberPad
         self.addressField.delegate = self;
     }
     
@@ -36,4 +40,38 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
         self.view.endEditing(true)
         return false
     }
+    
+    @IBAction func SignUpClick(sender: AnyObject) {
+   
+        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+        
+     let managedContext: NSManagedObjectContext = appDelegate!.managedObjectContext
+        
+        let entity = NSEntityDescription.entityForName("User", inManagedObjectContext: managedContext)
+        
+        let user = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        user.setValue(mailField.text, forKey:"mail")
+        user.setValue(passwordField.text, forKey:"password")
+        user.setValue(lastnameField.text, forKey:"nom")
+        user.setValue(firstnameField.text, forKey:"prenom")
+        user.setValue(phoneField.text, forKey:"telephone")
+        user.setValue(addressField.text, forKey:"adresse")
+ 
+        
+        do {
+            try managedContext.save() // 5
+        } catch {
+            print("Problème lors de la sauvegarde !")
+        }
+    }
+    
+    /*override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "Detail" {
+            //            if let indexpath = table.indexPathForSelectedRow {
+            let dvc = segue.destinationViewController as! detailViewController
+            dvc.detail = data[sender as! Int]
+            //           }
+        }
+    }*/
 }
