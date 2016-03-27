02306b0317edc6be7905ec879db4c13e3dc73d5c
tp <tp@tps-macbook-pro.local>
2016-03-27 23:38:46 +0200

    Sign In et Sign Up terminé avec erreur et annulation du segue en cas de problème Inscription impossible si le mail a déjà été utilisé /!\ Utilisateurs gardés en mémoire de l'émulateur

--- iOS-App/SignUpViewController.swift
+++ iOS-App/SignUpViewController.swift
@@ -41,37 +41,86 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
         return false
     }
     
-    @IBAction func SignUpClick(sender: AnyObject) {
-   
-        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
-        
-     let managedContext: NSManagedObjectContext = appDelegate!.managedObjectContext
-        
-        let entity = NSEntityDescription.entityForName("User", inManagedObjectContext: managedContext)
-        
-        let user = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
-        
-        user.setValue(mailField.text, forKey:"mail")
-        user.setValue(passwordField.text, forKey:"password")
-        user.setValue(lastnameField.text, forKey:"nom")
-        user.setValue(firstnameField.text, forKey:"prenom")
-        user.setValue(phoneField.text, forKey:"telephone")
-        user.setValue(addressField.text, forKey:"adresse")
- 
-        
-        do {
-            try managedContext.save() // 5
-        } catch {
-            print("Problème lors de la sauvegarde !")
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject!) -> Bool {
+        var segueShouldOccur : Int = 3 //variable pour le code d'erreur // 3 erreur inconnue
+        if identifier == "SignUp" {
+            //test si les champs sont remplis
+            if(mailField.text == "" || passwordField.text == "" || lastnameField.text == "" || firstnameField.text == "" || phoneField.text == "" || addressField.text == ""){
+                ////code erreur 1 champs incomplets
+                segueShouldOccur = 1
+            }
+            //test si le user existe
+            else {
+                //requete
+                let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+                let context: NSManagedObjectContext = appDel.managedObjectContext
+                let request = NSFetchRequest(entityName: "User")
+                request.predicate = NSPredicate(format: "mail = %@", mailField.text!)
+                request.returnsObjectsAsFaults = false
+                do {
+                    let resultats = try context.executeFetchRequest(request)
+                    //compte si il y a déjà des utilisateurs existant avec ce mail
+                    if resultats.count > 0 {
+                        ////code erreur 2 si le mail a déjà été utilisé par un compte
+                        segueShouldOccur = 2
+                    } else {
+                        ////code erreur 0 si le mail est valide
+                        segueShouldOccur = 0
+                    }
+                } catch {
+                    print("Echec de la requête Fetch !")
+                }
+            }
+            if segueShouldOccur != 0{
+                //test les codes d'erreur
+                var error : String = "";
+                if segueShouldOccur == 1 {
+                    error = "Please complete fields"
+                }
+                else if  segueShouldOccur == 2 {
+                    error = "User already exist"
+                }
+                else if segueShouldOccur == 3 {
+                    error = "Unknown error"
+                }
+                //popup erreur
+                let alert = UIAlertController (title: "Error", message: error, preferredStyle: UIAlertControllerStyle.Alert)
+                let OKAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                alert.addAction(OKAction)
+                presentViewController(alert, animated: true, completion: nil)
+                
+                // invalide le segue
+                return false
+            }
         }
+        
+        // valide le segue
+        return true
     }
     
-    /*override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "Detail" {
-            //            if let indexpath = table.indexPathForSelectedRow {
-            let dvc = segue.destinationViewController as! detailViewController
-            dvc.detail = data[sender as! Int]
-            //           }
-        }
-    }*/
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "SignUp" {
+            //requete ajout user
+            let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+            
+            let managedContext: NSManagedObjectContext = appDelegate!.managedObjectContext
+            
+            let entity = NSEntityDescription.entityForName("User", inManagedObjectContext: managedContext)
+            
+            let user = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+            
+            user.setValue(mailField.text, forKey:"mail")
+            user.setValue(passwordField.text, forKey:"password")
+            user.setValue(lastnameField.text, forKey:"nom")
+            user.setValue(firstnameField.text, forKey:"prenom")
+            user.setValue(phoneField.text, forKey:"telephone")
+            user.setValue(addressField.text, forKey:"adresse")
+            
+            
+            do {
+                try managedContext.save()
+            } catch {
+                print("Problème lors de la sauvegarde !")
+            }        }
+    }
 }
--- iOS-App/ViewController.swift
+++ iOS-App/ViewController.swift
@@ -45,6 +45,81 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject!) -> Bool {
+        var segueShouldOccur : Int = 4 //variable pour le code d'erreur // 3 erreur inconnue
+        if identifier == "SignIn" {
+            //teste si les champs ont été remplis
+            if(loginField.text == "" || passwordField.text == ""){
+                //// code erreur 1 champs incomplets 
+                segueShouldOccur = 1
+            }
+            else {
+                //requete avec l'adresse mail donnée
+                let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+                let context: NSManagedObjectContext = appDel.managedObjectContext
+                let request = NSFetchRequest(entityName: "User")
+                request.predicate = NSPredicate(format: "mail = %@", loginField.text!)
+                request.returnsObjectsAsFaults = false
+                do {
+                    let resultats = try context.executeFetchRequest(request)
+                    //teste si le user existe
+                    if resultats.count == 1 {
+                        for result in resultats as! [NSManagedObject] {
+                            let attendedPassword = result.valueForKey("password") as! String
+                            //teste si le password est bon
+                            if passwordField.text == attendedPassword  {
+                                ////code erreur 0 code bon
+                                segueShouldOccur = 0
+                            }
+                            else {
+                                ////code erreur 3 code invalide
+                                segueShouldOccur = 3
+                            }
+                        }
+                    } else {
+                        ////code erreur 2 le user n'existe pas
+                        segueShouldOccur = 2
+                    }
+                } catch {
+                    print("Echec de la requête Fetch !")
+                }
+                
+            }
+            if (!(segueShouldOccur == 0)) {
+                //teste les codes d'erreur
+                var error : String = "";
+                if segueShouldOccur == 1 {
+                    error = "Please complete fields"
+                }
+                else if  segueShouldOccur == 2 {
+                    error = "User does not exist"
+                }
+                else if segueShouldOccur == 3 {
+                    error = "Wrong password"
+                }
+                else if segueShouldOccur == 4 {
+                    error = "Unknown error"
+                }
+                //popup erreur
+                let alert = UIAlertController (title: "Error", message: error, preferredStyle: UIAlertControllerStyle.Alert)
+                
+                let OKAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                
+                alert.addAction(OKAction)
+                presentViewController(alert, animated: true, completion: nil)
+                
+                // invalide le segue
+                return false
+            }
+        }
+        
+        // valide le segue
+        return true
+    }
 
 }
 
